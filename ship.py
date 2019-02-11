153a84689cda0c21b0bc07745dcb03cd13df818b
Chris Kuo <chris.kuo@chriskuo.com>
2019-02-10 20:26:03 -0800

    Limits range of ship to the screen edges

--- ship.py
+++ ship.py
@@ -25,9 +25,9 @@ class Ship():
 	def update(self):
 		'''Update the ship's position based on the movement flag.'''
 		# update the ship's center value, not rect
-		if self.moving_right:
+		if self.moving_right and self.rect.right < self.screen_rect.right:
 			self.center += self.ai_settings.ship_speed_factor
-		if self.moving_left:
+		if self.moving_left and self.rect.left > self.screen_rect.left:
 			self.center -= self.ai_settings.ship_speed_factor
 
 		# update rect object from self.center
