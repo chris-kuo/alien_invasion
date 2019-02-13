e744cd6545eb1409c5b5e1585c3e92cf4c93c920
Chris Kuo <chris.kuo@chriskuo.com>
2019-02-13 00:02:31 -0800

    Change game screen size to 1200x800

--- settings.py
+++ settings.py
@@ -4,8 +4,8 @@ class Settings():
 	def __init__(self):
 		"""initialize the game's settings"""
 		# Screen settings
-		self.screen_width = 800
-		self.screen_height = 600
+		self.screen_width = 1200
+		self.screen_height = 800
 
 		# ship settings
 		self.bg_color = (230, 230, 230)
